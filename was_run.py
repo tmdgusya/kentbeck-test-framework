5cea1f67637e7ce6bb446b778d0fb4fff5e23d86
tmdgusya <tmdgusya@naver.com>
2022-09-12 23:20:49 +0900

    change test execution method that use execute run() method for executing test suite

--- was_run.py
+++ was_run.py
@@ -7,9 +7,12 @@ class WasRun:
     def testMethod(self):
         self.wasRun = 1
 
+    def run(self):
+        self.testMethod()
+
 
 if __name__ == "__main__":
     test = WasRun("testMethod")
     assert test.wasRun == None
-    test.testMethod()
+    test.run()
     assert test.wasRun == 1
